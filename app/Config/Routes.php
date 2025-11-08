93e5c007a1ed51e9367145242c40b3f42ec588e9
nixon24 <beanixon@hotmail.fr>
2025-11-08 23:14:45 +0100

    configuration BOOTSTRAP ok

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->group('auth', function (RouteCollection $routes){
+    $routes->match(['get','post'], 'login', 'Auth::login');
+});
